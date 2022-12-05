b5711ea7a9bf14be26b3bb7e29cd035103270217
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-05 11:43:46 -0500

    DisneySteps comments updated.

--- src/test/java/org/Globant/steps/DisneySteps.java
+++ src/test/java/org/Globant/steps/DisneySteps.java
@@ -43,42 +43,53 @@ public class DisneySteps {
     @Given("The user navigates to check the Disneyland Map")
     public void theUserNavigatesToCheckTheDisneylandMap() {
         Reporter.info("Navigating to the Disneyland Map.");
+        Reporter.info("Validate the Disneyland Map.");
         this.map = dashBoard.goToMapScreen();
-        Assert.assertTrue(map.disneylandMapIsDisplayed(), "The Disneyland map is not displayed.");
+        Assert.assertTrue(map.disneylandMapIsDisplayed(),
+                "The Disneyland map is not displayed.");
         Reporter.info("The Disneyland map is displayed.");
     }
 
     @When("taps the category list")
     public void tapsTheCategoryList() {
-        Assert.assertTrue(map.categoryButtonIsDisplayed(), "The Category button is not displayed");
+        Reporter.info("Validate the Category button.");
+        Assert.assertTrue(map.categoryButtonIsDisplayed(),
+                "The Category button is not displayed.");
         Reporter.info("The Category button is displayed.");
         map.clickOnCategoryList();
-        Assert.assertTrue(map.selectCategoryTitleIsDisplayed(), "The select Category title not displayed");
-        Reporter.info("The Category button is displayed.");
+        Assert.assertTrue(map.selectCategoryTitleIsDisplayed(),
+                "The select Category title is not displayed.");
+        Reporter.info("The Category title is displayed.");
     }
 
     @Then("the Hotels option should be available in the category list")
     public void theHotelsOptionShouldBeAvailableInTheCategoryList() {
-        Assert.assertTrue(map.hotelsCategoryButtonIsDisplayed(), "Hotels Category is not available");
+        Reporter.info("Validate the Hotels Category.");
+        Assert.assertTrue(map.hotelsCategoryButtonIsDisplayed(),
+                "The Hotels Category is not available.");
+        Reporter.info("The Hotels Category is available.");
     }
 
     @Given("The user is on the more other options Menu")
     public void theUserIsOnTheMoreOtherOptionsMenu() {
-        Reporter.info("Validate moreOtherOptions button");
-        Assert.assertTrue(dashBoard.moreOptionsButtonIsDisplayed(), "More Options button not displayed");
+        Reporter.info("Validate moreOtherOptions button.");
+        Assert.assertTrue(dashBoard.moreOptionsButtonIsDisplayed(),
+                "More Options button is not displayed.");
+        Reporter.info("The moreOtherOptions button is displayed.");
     }
 
     @When("checks all the options available")
     public void checksAllTheOptionsAvailable() {
-        Reporter.info("Start Navigation to More Options Screen");
+        Reporter.info("Navigating to More Options Screen.");
         this.moreOtherOptions = dashBoard.goToMoreOptionsScreen();
+        Reporter.info("Scroll down until bottom of the screen.");
         moreOtherOptions.scrollDownToPrivacyLegal();
         moreOtherOptions.moreOtherOptionsScreenAssertions();
     }
 
     @And("taps on the Privacy & Legal option")
     public void tapsOnThePrivacyLegalOption() {
-        Reporter.info("Start Navigation to Privacy & Legal Screen");
+        Reporter.info("Navigating to Privacy & Legal Screen");
         this.privacyLegalScreen = moreOtherOptions.goToPrivacyLegalScreen();
     }
 
@@ -89,21 +100,29 @@ public class DisneySteps {
 
     @Given("The Buy Tickets Reserve Dining button is available")
     public void theBuyTicketsReserveDiningButtonIsAvailable() {
-        Reporter.info("Validate 'Buy Tickets Reserve Dining' button");
-        Assert.assertTrue(dashBoard.buyTicketsReserveDiningButtonIsDisplayed(), "Check 'Buy Tickets Reserve Dining' button not displayed");
+        Reporter.info("Validate 'Buy Tickets Reserve Dining' button.");
+        Assert.assertTrue(dashBoard.buyTicketsReserveDiningButtonIsDisplayed(),
+                "The 'Buy Tickets Reserve Dining' button is not displayed.");
+        Reporter.info("The 'Buy Tickets Reserve Dining' button is displayed.");
     }
 
     @When("taps on the Buy Tickets Reserve Dining button")
     public void tapsOnTheBuyTicketsReserveDiningButton() {
-        Reporter.info("Start Navigation to 'Buy Tickets Reserve Dining' Screen");
+        Reporter.info("Navigating to 'Buy Tickets Reserve Dining' Screen.");
         this.buyTicketsReserveDiningScreen = dashBoard.goToBuyTicketsReserveDiningScreen();
-        Reporter.info("Validate 'Buy Tickets Reserve Dining' list");
-        Assert.assertTrue(buyTicketsReserveDiningScreen.buyTicketsReserveDiningListIsDisplayed(), "Check 'Buy Tickets Reserve Dining' list not displayed");
+        Reporter.info("Validate 'Buy Tickets Reserve Dining' list.");
+        Assert.assertTrue(
+                buyTicketsReserveDiningScreen.buyTicketsReserveDiningListIsDisplayed(),
+                "The 'Buy Tickets Reserve Dining' list is not displayed.");
+        Reporter.info("The 'Buy Tickets Reserve Dining' list is displayed.");
     }
 
     @Then("the Check Dining Availability button is displayed")
     public void theCheckDiningAvailabilityButtonIsDisplayed() {
-        Reporter.info("Validate 'Check Dining Availability' button");
-        Assert.assertTrue(buyTicketsReserveDiningScreen.checkDiningAvailabilityButtonIsDisplayed(), "Check 'Dining Availability' button not displayed");
+        Reporter.info("Validate 'Check Dining Availability' button.");
+        Assert.assertTrue(
+                buyTicketsReserveDiningScreen.checkDiningAvailabilityButtonIsDisplayed(),
+                "The 'Check Dining Availability' button is not displayed.");
+        Reporter.info("The 'Check Dining Availability' button.");
     }
 }
