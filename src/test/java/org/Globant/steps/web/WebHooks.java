7da792d5923c9a5ad7767a50db46e2f4e72eb2a1
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-06 22:08:00 -0500

    WebHooks applied.

---
+++ src/test/java/org/Globant/steps/web/WebHooks.java
@@ -0,0 +1,39 @@
+package org.Globant.steps.web;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.Globant.configuration.web.UniqueWebDriver;
+import org.Globant.pageObjects.web.HomePage;
+import org.Globant.reporting.Reporter;
+import org.openqa.selenium.WebDriver;
+
+import static java.lang.String.format;
+
+public class WebHooks {
+
+    private static UniqueWebDriver driver;
+
+    private static final String URL ="https://www.espnqa.com/?src=com&_adblock=true&espn=cloud";
+    private static final String BROWSER = "chrome";
+
+    @Before
+    public static void startBrowser() {
+        driver = new UniqueWebDriver(BROWSER);
+        Reporter.info("Deleting all the cookies");
+        driver.getDriver().manage().deleteAllCookies();
+        Reporter.info(format("Navigating to %s", URL));
+        Reporter.info("Navigating the ESPN Landing Page");
+        driver.getDriver().get(URL);
+        driver.getDriver().manage().window().maximize();
+        //home.closeBanner();
+    }
+
+    @After
+    public void teardown() { driver.getDriver().quit(); }
+
+
+    public static WebDriver getDriver() {
+        return driver.getDriver();
+    }
+
+}
