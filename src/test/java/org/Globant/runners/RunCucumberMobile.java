cfe53582432dc730cbcfa3e8e6e19bb2921c3a14
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-07 01:53:09 -0500

    suiteTest.xml and runners updated.

--- src/test/java/org/Globant/runners/RunCucumberMobile.java
+++ src/test/java/org/Globant/runners/RunCucumberMobile.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/featuresx",
+        features = "src/test/resources/features",
         glue = {"org.Globant.steps.mobile"}
 )
 
