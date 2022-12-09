a291507c4a06107ac397dc5a8cdc727f363c0483
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-09 17:44:17 -0500

    JavaDoc updated.

--- src/main/java/org/Globant/configuration/mobile/UniqueMobileDriver.java
+++ src/main/java/org/Globant/configuration/mobile/UniqueMobileDriver.java
@@ -3,18 +3,29 @@ package org.Globant.configuration.mobile;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
+
+/**
+ * Represents the Mobile driver used to control the Android application.
+ *
+ * @author juancamilo.gonzalez
+ * @version 1.0
+ */
 public class UniqueMobileDriver {
     private final AndroidDriver<AndroidElement> driver;
 
+    /**
+     * Constructor method for the UniqueMobileDriver class.
+     *
+     * @param driver : The driver that controls the behaviour
+     * of an android application.
+     */
     public UniqueMobileDriver(AndroidDriver<AndroidElement> driver) {
         this.driver = driver;
     }
 
     /**
-     * return the driver.
-     *
-     * @return driver
-     * @author Arley.Bolivar
+     * Returns the driver.
+     * @return The driver that controls the behaviour of an android application.
      */
     public AndroidDriver<AndroidElement> getDriver() {
         return this.driver;
