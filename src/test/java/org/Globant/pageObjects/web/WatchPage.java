4c63b75aa0c33d29ea3fa2e5ba9f80432fa46259
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-08 23:39:10 -0500

    JavaDoc updated.

--- src/test/java/org/Globant/pageObjects/web/WatchPage.java
+++ src/test/java/org/Globant/pageObjects/web/WatchPage.java
@@ -27,42 +27,71 @@ public class WatchPage extends BasePage {
     }
 
     /**
-     * The first Carousel on the watch page
+     * The first Carousel on the watch page.
      */
     @FindBy(css="#fittPageContainer > section > div:nth-child(1) > section")
     private WebElement firstCarousel;
 
-    @FindBy (css = "section.Carousel")
-    private List<WebElement> carouselList;
-
+    /**
+     * The second card in the first Carousel on the watch page.
+     */
     @FindBy(css="#fittPageContainer > section > div:nth-child(1) > section > div.Carousel__Wrapper.relative.Carousel__Wrapper--canScrollRight > div > div > ul > li:nth-child(2)")
     private WebElement secondCardFirstCarousel;
 
+    /**
+     * The close button in the TV provider window.
+     */
     @FindBy(css=".lightbox__closebtn")
     private WebElement xButton;
 
+    /**
+     * Checks if the first carousel is displayed.
+     * @return True if the first carousel is displayed,
+     * otherwise the return is false.
+     */
     public boolean isFirstCarouselDisplayed() {
         super.waitForVisibility(firstCarousel);
         return firstCarousel.isDisplayed();
     }
+
+    /**
+     * Checks if the second card in the first carousel is displayed.
+     * @return True if the second card in the first carousel is displayed,
+     * otherwise the return is false.
+     */
     public boolean isSecondCardDisplayed() {
         super.waitForVisibility(secondCardFirstCarousel);
         return secondCardFirstCarousel.isDisplayed();
     }
+
+    /**
+     * Checks if the close button in TV provider window is displayed.
+     * @return True if close button in TV provider window is displayed,
+     * otherwise the return is false.
+     */
     public boolean isxButtonDisplayed() {
         super.waitForVisibility(xButton);
         return xButton.isDisplayed();
     }
 
-    public void clickOnxButton(){
-        super.clickElement(xButton);
-    }
-
+    /**
+     * Clicks on the second card in the first carousel.
+     */
     public void clickOnSecondCard(){
         super.waitForVisibility(secondCardFirstCarousel);
         super.clickElement(secondCardFirstCarousel);
     }
 
+    /**
+     * Clicks on the close button in TV provider window is displayed.
+     */
+    public void clickOnxButton(){
+        super.clickElement(xButton);
+    }
+
+    /**
+     * Takes the user to the ESPN home page.
+     */
     public void returnToHome(){
         Reporter.info("Navigating to the ESPN Landing Page.");
         super.getDriver().navigate().back();
