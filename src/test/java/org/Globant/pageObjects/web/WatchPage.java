cc13b89d09afd9290ff7c0c7fce36eb9ebb93da3
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-08 23:39:42 -0500

    JavaDoc updated.

--- src/test/java/org/Globant/pageObjects/web/WatchPage.java
+++ src/test/java/org/Globant/pageObjects/web/WatchPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
-
 /**
  * Represents the ESPN watch page.
  *
