1bc066b763b393c758236eecf5e1aef316412a24
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-09 00:17:04 -0500

    JavaDoc updated.

--- src/main/java/org/Globant/configuration/mobile/MobileOperations.java
+++ src/main/java/org/Globant/configuration/mobile/MobileOperations.java
@@ -1,12 +1,9 @@
 package org.Globant.configuration.mobile;
 
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import io.appium.java_client.touch.offset.PointOption;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,15 +13,21 @@ import java.time.Duration;
 
 import static org.openqa.selenium.support.PageFactory.initElements;
 
+/**
+ * Represents the Mobile operations done by an Android application.
+ *
+ * @author juancamilo.gonzalez
+ * @version 1.0
+ */
 public class MobileOperations {
     private final AndroidDriver<AndroidElement> driver;
     private final WebDriverWait wait;
 
     /**
-     * Constructor method for standard screens object.
+     * Constructor method for the MobileOperations class.
      *
-     * @param driver : AndroidDriver
-     * @author Arley.Bolivar, Hans.Marquez
+     * @param driver : The driver that controls the behaviour
+     * of an android application.
      */
     public MobileOperations(AndroidDriver<AndroidElement> driver) {
         this.driver = driver;
@@ -33,15 +36,17 @@ public class MobileOperations {
                 driver, Duration.ofSeconds(0)), this);
     }
 
+    /**
+     * Gets the driver for an Android application.
+     * @return An Android element representing the driver for an Android application.
+     */
     public AndroidDriver<AndroidElement> getDriver() {
         return this.driver;
     }
 
     /**
-     * Scroll down (From Top to Bottom).
-     *
-     * @param swipes the swipes
-     * @author Hans.Marquez
+     * Scrolls down the screen (From Top to Bottom).
+     * @param swipes The swipes.
      */
     public void scrollDown(int swipes) {
         String locator = "new UiScrollable(new UiSelector().resourceIdMatches(\".*ontainer.*\")).flingToEnd(1)";
@@ -49,10 +54,9 @@ public class MobileOperations {
     }
 
     /**
-     * Scroll.
-     *
-     * @param locator the locator
-     * @param swipes  the swipes
+     * Scrolls the screen.
+     * @param locator The locator.
+     * @param swipes  The swipes.
      */
     public void scroll(String locator, int swipes) {
         int swipesAmount = 0;
@@ -67,23 +71,8 @@ public class MobileOperations {
     }
 
     /**
-     * Swipe vertical.
-     *
-     * @param percentage of swipe
-     */
-    @SuppressWarnings({"rawtypes", "unused"})
-    public void swipeVertical(float percentage) {
-        Dimension windowSize = getDriver().manage().window().getSize();
-        TouchAction ta = new TouchAction(getDriver());
-        ta.press(PointOption.point(207, 582)).moveTo(PointOption.point(
-                8,-360)).release().perform();
-    }
-
-    /**
      * Wrapper for click event.
-     *
-     * @param element : AndroidElement
-     * @author Hans.Marquez
+     * @param element : An Android element.
      */
     public void click(AndroidElement element) {
         WebDriverWait wait = new WebDriverWait(driver, 15L);
@@ -92,10 +81,9 @@ public class MobileOperations {
     }
 
     /**
-     * Wrapper for Visibility event.
-     *
-     * @param element : AndroidElement
-     * @author Hans.Marquez
+     * Wrapper for Visibility event using a default timeout.
+     * @param element : An Android element.
+     * @return True if the Android element is available, otherwise the return is false.
      */
     public boolean isElementAvailable(AndroidElement element) {
         WebDriverWait wait = new WebDriverWait(driver, 45L);
@@ -108,11 +96,10 @@ public class MobileOperations {
     }
 
     /**
-     * Wrapper for Visibility event.
-     *
-     * @param element : AndroidElement
-     * @param timeout : int
-     * @author Hans.Marquez
+     * Wrapper for Visibility event using a custom timeout.
+     * @param element : An Android element.
+     * @param timeout : Timeout for visibility.
+     * @return True if the Android element is available, otherwise the return is false.
      */
     public boolean isElementAvailable(AndroidElement element, long timeout) {
         WebDriverWait wait = new WebDriverWait(driver, timeout);
@@ -123,6 +110,4 @@ public class MobileOperations {
             return false;
         }
     }
-
-
 }
