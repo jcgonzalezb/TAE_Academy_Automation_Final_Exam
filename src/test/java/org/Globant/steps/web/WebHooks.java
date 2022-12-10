e433ddce75c8859f394839a385ba03591adb8c97
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-09 22:17:11 -0500

    JavaDocs updated. WebHooks updated.

--- src/test/java/org/Globant/steps/web/WebHooks.java
+++ src/test/java/org/Globant/steps/web/WebHooks.java
@@ -8,12 +8,22 @@ import org.openqa.selenium.WebDriver;
 
 import static java.lang.String.format;
 
+
+/**
+ * Represents the Hooks used for the EspnSteps class.
+ *
+ * @author juancamilo.gonzalez
+ * @version 1.0
+ */
 public class WebHooks {
 
     private static UniqueWebDriver driver;
     private static final String URL ="https://www.espnqa.com/?src=com&_adblock=true&espn=cloud";
     private static final String BROWSER = "chrome";
 
+    /**
+     * Starts the browsers properly.
+     */
     @Before
     public static void startBrowser() {
         driver = new UniqueWebDriver(BROWSER);
@@ -25,9 +35,16 @@ public class WebHooks {
         driver.getDriver().manage().window().maximize();
     }
 
+    /**
+     * Closes the web browser after completing the test.
+     */
     @After
     public void closeBrowser() { driver.getDriver().quit(); }
 
+    /**
+     * Returns the driver.
+     * @return An WebDriver element representing the driver for web browser.
+     */
     public static WebDriver getDriver() {
         return driver.getDriver();
     }
