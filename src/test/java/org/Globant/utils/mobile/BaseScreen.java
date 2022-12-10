60ec40cd70e9d290e9a60e1b4dc28671fef2b4f2
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-12-09 21:57:10 -0500

    JavaDocs updated. BaseScreen updated.

--- src/test/java/org/Globant/utils/mobile/BaseScreen.java
+++ src/test/java/org/Globant/utils/mobile/BaseScreen.java
@@ -4,13 +4,20 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.Globant.configuration.mobile.MobileOperations;
 
+
+/**
+ * Represents the Base screen.
+ *
+ * @author juancamilo.gonzalez
+ * @version 1.0
+ */
 public class BaseScreen extends MobileOperations {
 
     /**
      * Constructor method for standard screens object.
      *
-     * @param driver : AndroidDriver
-     * @author Arley.Bolivar, Hans.Marquez
+     * @param driver : The driver that controls the behaviour
+     * of an android application.
      */
     public BaseScreen(AndroidDriver<AndroidElement> driver) {
         super(driver);
